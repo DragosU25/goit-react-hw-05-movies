8db9428cb62e81cf7867ed5584dcde30610bb927
DragosU25 <149568004+DragosU25@users.noreply.github.com>
2024-07-27 20:24:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Note the import from 'react-dom/client'
 import App from 'components/App';
-import { ImageProvider } from 'components/Context/Context';
 
 // Create a root
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // Render the App component into the root
-root.render(
-  <ImageProvider>
-    <App />
-  </ImageProvider>
-);
+root.render(<App />);
